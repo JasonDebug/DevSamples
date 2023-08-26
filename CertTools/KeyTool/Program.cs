ac97a47619e3df8caf45b537d6d715c604e3cb38
Jason <38758441+JasonDebug@users.noreply.github.com>
2023-08-26 00:20:44 -0400

    Update Program.cs

--- CertTools/KeyTool/Program.cs
+++ CertTools/KeyTool/Program.cs
@@ -20,22 +20,22 @@ namespace KeyTool.net
                 "Microsoft Strong Cryptographic Provider",
                 "Microsoft Enhanced Cryptographic Provider v1.0",
                 "Microsoft Enhanced RSA and AES Cryptographic Provider"
-            };
+        };
+        static string MachineKeysFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), @"Microsoft\Crypto\RSA\MachineKeys");
 
         static void Main(string[] args)
         {
             // Init output file
             File.WriteAllText(outputFile, $"Container Name, File Name, Public Key{Environment.NewLine}");
 
-            // Get keyfiles for existing certs
-            FindCertificateKeyFiles2();
+            // Get keyfiles and public keys for existing certs
             FindCertificateKeyFiles();
 
             // Find duplicate key files
             FindDuplicateKeyFiles();
 
-            // We could capture the duplicates for a count, etc, but with 80GB of 3KB files -- I don't want to make a Dictionary/List that large
-            // Use the output file to delete the dupes or delete them while going via File.Delete() etc
+            // Compare with physical key files.  Any files in here that we can't see from APIs is probably orphaned
+            FindOrphanedKeyFiles();
         }
 
         static void Log(string message)
@@ -71,12 +71,16 @@ namespace KeyTool.net
             int num = (int)(2 * dwDataLen);
             pbData = new StringBuilder(num);
             dwFlags2 = 1;
+            uint index = 0;
+            uint dupes = 0;
 
             while (CryptoAPI.CryptGetProvParam(hProv, dwParam, pbData, ref dwDataLen, dwFlags2))
             {
+                index++;
                 dwFlags2 = 0;
 
                 var containerName = pbData.ToString();
+                var keyFile = GetUniqueContainerName(containerName);
                 var publicKey = GetPublicKey(containerName);
 
                 if (!String.IsNullOrEmpty(publicKey))
@@ -84,20 +88,29 @@ namespace KeyTool.net
                     if (publicKeys.Contains(publicKey))
                     {
                         // Dupe key, log to dupe list
-                        var keyFile = GetUniqueContainerName(containerName);
                         if (!certKeyFiles.Contains(keyFile))
                         {
-                            File.AppendAllText(outputFile, $"{containerName}, {GetUniqueContainerName(containerName)}, {publicKey}{Environment.NewLine}");
+                            // We could capture the duplicates for a count, etc, but with 80GB of 3KB files -- we don't want to make a Dictionary/List
+                            // Instead, we'd do this realtime and delete the dupes as we go or delete them after the fact via an external file
+                            // DELETE_DUPES is false, so we don't delete anything, but we could by setting that to true
+
+                            File.AppendAllText(outputFile, $"{containerName}, {keyFile}, {publicKey}{Environment.NewLine}");
+                            dupes++;
 
                             if (DELETE_DUPES)
                             {
-                                var fileToDelete = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), @"Microsoft\Crypto\RSA\MachineKeys", GetUniqueContainerName(containerName));
+                                var fileToDelete = Path.Combine(MachineKeysFilePath, keyFile);
+
+                                // Delete the file !!
                                 //File.Delete(fileToDelete);
                             }
                         }
                     }
                     else
                     {
+                        // Add the file to the list for the last step in cleanup
+                        certKeyFiles.Add(keyFile);
+
                         publicKeys.Add(publicKey);
                     }
                 }
@@ -106,6 +119,8 @@ namespace KeyTool.net
             {
                 CryptoAPI.CryptReleaseContext(hProv, 0);
             }
+
+            Log($"Found {index} keys -- {dupes} duplicates.");
         }
 
         static string GetUniqueContainerName(string containername)
@@ -141,7 +156,7 @@ namespace KeyTool.net
             return string.Empty;
         }
 
-        static string GetPublicKey(string containername, bool verbose = false)
+        static string GetPublicKey(string containername)
         {
             uint dwProvType = CryptoAPI.PROV_RSA_FULL;
             uint dwBlobType = 6;    // PUBLICKEYBLOB
@@ -163,8 +178,7 @@ namespace KeyTool.net
                             {
                                 if (!CryptoAPI.CryptExportKey(hKey, zero, dwBlobType, 0, null, ref dwDataLen))
                                 {
-                                    if (verbose)
-                                        Log(Marshal.GetLastWin32Error());
+                                    Log(Marshal.GetLastWin32Error());
 
                                     return string.Empty;
                                 }
@@ -177,9 +191,6 @@ namespace KeyTool.net
                                     }
                                     else
                                     {
-                                        if (verbose)
-                                            Log($"\tPublic key:\r\n{Convert.ToBase64String(array)}");
-
                                         return Convert.ToBase64String(array);
                                     }
                                 }
@@ -200,7 +211,10 @@ namespace KeyTool.net
             }
         }
 
-        static void FindCertificateKeyFiles2()
+        /// <summary>
+        /// Populate the certKeyFiles list with all the cert key files
+        /// </summary>
+        static void FindCertificateKeyFiles()
         {
             using (var store = new X509Store(StoreName.My, StoreLocation.LocalMachine))
             {
@@ -208,139 +222,50 @@ namespace KeyTool.net
 
                 foreach (var certificate in store.Certificates)
                 {
-                    // These console outputs are just for debugging purposes
-                    //Log($"Subject: {certificate.Subject}");
-                    //Log($"\tIssuer: {certificate.Issuer}");
+                    // Protect via public key as well
+                    publicKeys.Add(Convert.ToBase64String(certificate.GetPublicKey()));
 
                     if (!certificate.HasPrivateKey)
-                    {
-                        //Log("\tNo private key associated with the certificate.");
                         continue;
-                    }
 
-                    // Check if the private key is of type RSACryptoServiceProvider
-                    if (certificate.PrivateKey is RSACryptoServiceProvider rsaCsp)
-                    {
-                        // Check the ProviderType
-                        switch (rsaCsp.CspKeyContainerInfo.ProviderType)
-                        {
-                            case 1:
-                                Log($"\tKey provider: CSP - PROV_RSA_FULL");
-                                break;
-                            case 12:
-                                Log($"\tKey provider: CSP - PROV_RSA_AES");
-                                break;
-                            case 24:
-                                Log($"\tKey provider: KSP - PROV_RSA_AES");
-                                break;
-                            default:
-                                Log($"\tKey provider: Unknown provider type: {rsaCsp.CspKeyContainerInfo.ProviderType}");
-                                break;
-                        }
-                    }
-                    else if (certificate.PrivateKey is RSACng)
-                    {
-                        // Key provider: KSP
+                    string keyFile = null;
 
+                    if (certificate.PrivateKey is RSACng rsaCng)
+                    {
                         // If it's RSACng, it's using KSP, and not in the MachineKeys folder (normally)
                         // We're still going to protect the file by adding it to the protected cert keyfiles list
-                        var keyFile = ((RSACng)certificate.PrivateKey).Key.UniqueName;
-
-                        if (!string.IsNullOrEmpty(keyFile))
-                        {
-                            // Path is either explicit, or %PROGRAMDATA%\Microsoft\Crypto\Keys\
-                            certKeyFiles.Add(keyFile.Substring(keyFile.LastIndexOf('\\') + 1));
-                        }
+                        keyFile = rsaCng.Key.UniqueName;
                     }
-
-                    uint dataSize = 0;
-                    if (!CryptoAPI.CertGetCertificateContextProperty(certificate.Handle, CryptoAPI.CERT_KEY_PROV_INFO_PROP_ID, IntPtr.Zero, ref dataSize))
+                    else if (certificate.PrivateKey is RSACryptoServiceProvider rsaCsp)
                     {
-                        Log(Marshal.GetLastWin32Error());
+                        // This should work for CSPs but I cannot get a cert to import that way to test
+                        keyFile = rsaCsp.CspKeyContainerInfo.UniqueKeyContainerName;
                     }
 
-                    IntPtr dataBuffer = Marshal.AllocHGlobal((int)dataSize);
-                    if (!CryptoAPI.CertGetCertificateContextProperty(certificate.Handle, CryptoAPI.CERT_KEY_PROV_INFO_PROP_ID, dataBuffer, ref dataSize))
+                    if (!string.IsNullOrEmpty(keyFile) && !certKeyFiles.Contains(keyFile))
                     {
-                        Log(Marshal.GetLastWin32Error());
+                        // Path is generally either fully qualified, or %PROGRAMDATA%\Microsoft\Crypto\Keys\
+                        certKeyFiles.Add(keyFile.Substring(keyFile.LastIndexOf('\\') + 1));
                     }
-
-                    CRYPT_KEY_PROV_INFO provInfo = (CRYPT_KEY_PROV_INFO)Marshal.PtrToStructure(dataBuffer, typeof(CRYPT_KEY_PROV_INFO));
-                    string containerName = provInfo.pwszContainerName;
-
-                    Log($"\tKey Container Name: {containerName}");
-                    Log($"\tFilename: {GetUniqueContainerName(containerName)}");
-
-                    Marshal.FreeHGlobal(dataBuffer);
                 }
             }
         }
 
-        static Hashtable FindCertificateKeyFiles()
+        static void FindOrphanedKeyFiles()
         {
-            IntPtr zero = IntPtr.Zero;
-            IntPtr intPtr = IntPtr.Zero;
-            IntPtr intPtr2 = IntPtr.Zero;
-            uint pcbData = 0u;
-            Hashtable hashtable = new Hashtable();
-            X509Certificate2 x509Certificate = null;
-            uint dwFlags = CryptoAPI.CERT_SYSTEM_STORE_LOCAL_MACHINE | 0x8000u | 0x4000u;
-            zero = CryptoAPI.CertOpenStore(CryptoAPI.CERT_STORE_PROV_SYSTEM, CryptoAPI.X509_ASN_ENCODING | CryptoAPI.PKCS_7_ASN_ENCODING, IntPtr.Zero, dwFlags, "MY");
-            if (zero == IntPtr.Zero)
+            uint index = 0;
+            foreach (var file in Directory.GetFiles(MachineKeysFilePath))
             {
-                Log("Couldn't get certificate store handle");
-                return hashtable;
-            }
+                var keyFile = Path.GetFileName(file);
 
-            while ((intPtr = CryptoAPI.CertEnumCertificatesInStore(zero, intPtr)) != IntPtr.Zero)
-            {
-                if (CryptoAPI.CertGetCertificateContextProperty(intPtr, 2, IntPtr.Zero, ref pcbData))
+                if (!certKeyFiles.Contains(keyFile))
                 {
-                    intPtr2 = Marshal.AllocHGlobal((int)pcbData);
-                    if (CryptoAPI.CertGetCertificateContextProperty(intPtr, 2, intPtr2, ref pcbData))
-                    {
-                        CRYPT_KEY_PROV_INFO cRYPT_KEY_PROV_INFO = (CRYPT_KEY_PROV_INFO)Marshal.PtrToStructure(intPtr2, typeof(CRYPT_KEY_PROV_INFO));
+                    index++;
 
-                        var keyFile = GetUniqueContainerName(cRYPT_KEY_PROV_INFO.pwszContainerName);
-
-                        Log($"Protecting key file: {keyFile}");
-                        certKeyFiles.Add(keyFile);
-
-                        x509Certificate = new X509Certificate2(intPtr);
-                        CERTPROPS_INFO value = new CERTPROPS_INFO(x509Certificate.GetCertHash(), x509Certificate.Subject);
-
-                        //Console.Write("Public Key: ");
-
-                        var publicKey = Convert.ToBase64String(x509Certificate.GetPublicKey());
-
-                        if (publicKeys.Contains(publicKey))
-                        {
-                            Log("Duplicate, skipping");
-                        }
-                        else
-                        {
-                            publicKeys.Add(publicKey);
-                        }
-                    }
-                }
-                else
-                {
-                    Log(Marshal.GetLastWin32Error());
+                    //File.Delete(file);
                 }
             }
-            if (intPtr2 != IntPtr.Zero)
-            {
-                Marshal.FreeHGlobal(intPtr2);
-            }
-            if (intPtr != IntPtr.Zero)
-            {
-                CryptoAPI.CertFreeCertificateContext(intPtr);
-            }
-            if (zero != IntPtr.Zero)
-            {
-                CryptoAPI.CertCloseStore(zero, 0u);
-            }
-            return hashtable;
+            Console.WriteLine($"Found {index} orphaned key files.");
         }
     }
 }
